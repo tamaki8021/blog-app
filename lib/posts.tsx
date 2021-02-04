f8ff6e222994a6f6c2c4ee345435f67c5dce278c
tamaki <hbk2002320@gmail.com>
2021-02-04 11:23:41 +0900

    feat/github-repository-md-api

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -1,8 +1,11 @@
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
-import remark from 'remark'
+import remark, { stringify } from 'remark'
 import html from 'remark-html'
+import fetch from 'node-fetch'
+
+const base64 = require('js-base64').Base64
 
 const postsDirectory = path.join(process.cwd(), 'posts')
 
@@ -36,8 +39,16 @@ export function getSortedPostsData() {
   })
 }
 
-export function getAllPostIds() {
-  const fileNames = fs.readdirSync(postsDirectory)
+export async function getAllPostIds() {
+  // const fileNames = fs.readdirSync(postsDirectory)
+
+  //gitのレポジトリーからapiを使ってMarkdownファイルをとってくる処理
+  const repoUrl = 'https://api.github.com/repos/tamaki8021/blog-app/contents/posts'
+  const response = await fetch(repoUrl)
+  const files = await response.json()
+  const fileNames = files.map((file) => file.name)
+
+
 
   // Returns an array that looks like this:
   // [
@@ -62,8 +73,16 @@ export function getAllPostIds() {
 }
 
 export async function getPostData(id: string) {
-  const fullPath = path.join(postsDirectory, `${id}.md`)
-  const fileContents = fs.readFileSync(fullPath, 'utf8')
+  // const fullPath = path.join(postsDirectory, `${id}.md`)
+  // const fileContents = fs.readFileSync(fullPath, 'utf8')
+
+
+  //gitのレポジトリーからapiを使ってMarkdownファイルをとってくる処理
+  const repoUrl = `https://api.github.com/repos/tamaki8021/blog-app/contents/posts/${id}.md`
+  const response = await fetch(repoUrl)
+  const file = await response.json()
+  const fileContents = base64.decode(file.content)
+
 
   // Use gray-matter to parse the post metadata section
   const matterResult = matter(fileContents)
